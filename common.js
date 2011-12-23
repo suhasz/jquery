a3e39bfc7260a10b1d0dccfbd80559e560d90376
vinu123 <wenothe@gmail.com>
2011-12-23 16:20:50 +0530

    Changed structure

--- common.js
+++ common.js
@@ -1,9 +1,20 @@
 
-//Adding numbers to the list
-
+/* Custom Relevant Functions */
 
 $(function(){
+
+	//Adding numbers to the list
 	$('.pluginList > li').each(function(i){
 		$(this).prepend('<span>'+(i+=1)+'</span>');
 	});
-});
+	
+	//Grabbing the link & displays beside it
+/* 	var linkValue;
+	$('.pluginList > li').hover(function(){
+		$('.pluginList > li').each(function(){
+			var linkValue = $(this).find('a').attr('href');
+			alert(linkValue);
+		});
+	}); */
+
+});//document ready
